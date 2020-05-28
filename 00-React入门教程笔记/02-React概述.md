42bcf964372f423ccb4186b9e095c0fab6a033c7
itThreeDay <2939496363@qq.com>
2020-05-28 18:20:51 +0800

    渲染之前判断胜负在render方法中做判断；

--- 00-React入门教程笔记/02-React概述.md
+++ 00-React入门教程笔记/02-React概述.md
@@ -1,6 +1,6 @@
 ## React是什么？
 -   React 是一个声明式，高效且灵活的用于构建用户界面的 JavaScript 库.使用 React 可以将一些简短、独立的代码片段组合成复杂的 UI 界面，这些代码片段被称作“组件”
-## 组件声明：
+## 组件声明：JSX语法
 -   加载react  react-dom
 -   class 继承方式；React.Component
 -   render方法是负责渲染内容
@@ -16,19 +16,20 @@
 ## props传值
 父组件通过props 传值给子组件；
 -   在调用子组件的地方，用设置属性的方式，设置props的参数名，注意用{}  <sqaue value={10}/>;
+-   子组件通过 this.props.value 直接使用
 
 ## state 统一状态
 -   在组件中可以使用state存储状态；
 -   使用多遍的数据时，多使用state存储数据；
--   使用props从父组件传递数据给子组件，子组件把数据存放到state中； this.state={value:10};
+-   使用props从父组件传递数据给子组件，子组件把数据存放到state中；一般情况在构造中设置state：
+     this.state={value:10};
 -   展示的数据来源是state，不再直接使用props；  {this.state.value}
--   改变数据时，改修state，react会重新渲染页面，修改数据展示；  this.setState({value:20});
+-   改变数据时，改修state，react会重新渲染页面，修改数据展示；this.setState({value:20});
 
 ## 事件
 -   在组件中的DOM元素中 直接用js事件属性的方式绑定事件 
     <div onclick={()=>{this.setState({value:20})}}></div>
     注意{}包含js事件函数
-
 
 ## JSX中的注释：
 -   如果是在DOM之外，用/*这种事DOM之外*/
@@ -48,3 +49,8 @@ ReactDOM.render(
     document.getElementById('demo')
 );
 ```
+
+## 子组件通过事件触发父组件函数
+-   父组件传递给子组件一个函数
+-   子组件事件触发时，调用该函数；
+-   通过这种方式，把子组件的事件通知给父组件。
--- src/index.js
+++ src/index.js
@@ -1,36 +1,44 @@
-import React  from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css"
-class Square extends React.Component{   
-    render(){        
+class Square extends React.Component {
+    render() {
         return (
-            <button className="square" onClick={()=>{this.props.onClick()}}>
+            <button className="square" onClick={() => { this.props.onClick() }}>
                 {/*当点击时，触发通过props接收的父组件的方法。 */}
-                {this.props.value} { /*使用props显示父组件中保存的数据*/}                   
+                {this.props.value} { /*使用props显示父组件中保存的数据*/}
             </button>
         )
     }
 }
-class Board extends React.Component{
-    constructor(args){
+class Board extends React.Component {
+    constructor(args) {
         super(args);
-        this.state={
-            squares:Array(9).fill(null)
+        this.state = {
+            squares: Array(9).fill(null),
+            isNextX:true
         }
     }
-    renderSquare(index){
+    renderSquare(index) {
         return (
-            <Square 
-            value={this.state.squares[index]}
-            onClick={()=>{this.handleClick(index)}}
-            /> 
+            <Square
+                value={this.state.squares[index]}
+                onClick={() => { this.handleClick(index) }}
+            />
             /*这里通过props方式传递参数value，
             以及函数onClick；
             此处子组件的展示数据部分就跟父组件的state数据绑定了*/
         )
     }
-    render(){
-        const status="next player: X";
+    render() {
+        let status = "next player: X";
+        //每次渲染页面之前，判断时候已经已经胜负已分；
+        let winner = calculateWinner(this.state.squares);
+        if (winner) {
+            status = "winner:" + winner;
+        } else {
+            status = "next player:" + (this.state.isNextX ? "X" : "O");
+        }
         return (
             <div>
                 <div className="status">{status}</div>
@@ -52,15 +60,29 @@ class Board extends React.Component{
             </div>
         )
     }
-    handleClick(index){
-        const squares=this.state.squares.slice();
-        squares[index]="X";
-        this.setState({squares})
+    handleClick(index) {
+        //复制一份新的数据
+        const squares = this.state.squares.slice();
+        if(calculateWinner(squares)||squares[index]){
+            return;
+            /**
+             * 这是当已经有了胜者，封盘。点击不能再落子；
+             * 或者点击的位置已经有子，无法在此处再落子
+             */
+        }
+        //把落子的情况记录到数组中；
+        squares[index] = this.state.isNextX ? "X" : "O";
+
+        //用新的数据替换旧数据，这里用完整替换的方式，是为了“时间穿梭”后续理解
+        this.setState({
+            squares,
+            isNextX:!this.state.isNextX
+        })
     }
 }
 
-class Game extends React.Component{
-    render(){
+class Game extends React.Component {
+    render() {
         return (
             <div className="game">
                 <div className="game-board">
@@ -75,6 +97,28 @@ class Game extends React.Component{
     }
 }
 ReactDOM.render(
-    <Game/>,
+    <Game />,
     document.getElementById("root")
 )
+/**
+ * 判断胜负；返回X O  null
+ */
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
